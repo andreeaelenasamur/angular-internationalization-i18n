35a18d034f24282a6aee67b086c1d05c5b596e73
Andreea <andelenasamur@gmail.com>
2025-01-27 10:25:11 +0100

    write and read cookies

---
+++ src/app/service/language.service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+
+  cookie = inject(SsrCookieService);
+
+  changeLang( lang: string ) {
+    this.cookie.set('lang', lang);
+
+    console.log({lang})
+
+    // TODO: Cambio de idioma
+
+  }
+
+}
