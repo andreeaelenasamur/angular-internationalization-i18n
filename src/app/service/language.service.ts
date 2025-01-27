52768e7ec45341f3d8f1cb59239880fdbcd76916
Andreea <andelenasamur@gmail.com>
2025-01-27 12:56:58 +0100

    use cookies from the server

--- src/app/service/language.service.ts
+++ src/app/service/language.service.ts
@@ -1,15 +1,21 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { inject, Injectable, InjectionToken, signal } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { SsrCookieService } from 'ngx-cookie-service-ssr';
+
+export const SERVER_LANG_TOKEN = new InjectionToken<string>('SERVER_LANG_TOKEN');
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
-
   cookie = inject(SsrCookieService);
   translate = inject(TranslateService);
-  currentLang = signal('');
+
+  langServer = inject(SERVER_LANG_TOKEN, {
+    optional: true
+  });
+
+  currentLang = signal(this.langServer ?? 'en');
 
   changeLang( lang: string ) {
     this.cookie.set('lang', lang);
